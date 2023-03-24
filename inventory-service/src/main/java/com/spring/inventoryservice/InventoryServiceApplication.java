d25cb1f6d6c821419f0bba70a67e37ae3253e16b
ardaerpak <arda-erpak@hotmail.com>
2023-03-24 21:35:19 +0300

    inventory service

---
+++ inventory-service/src/main/java/com/spring/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,34 @@
+package com.spring.inventoryservice;
+
+import com.spring.inventoryservice.model.Inventory;
+import com.spring.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_13_red");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
+
+}
---
+++ inventory-service/src/main/java/com/spring/inventoryservice/service/InventoryService.java
@@ -0,0 +1,19 @@
+package com.spring.inventoryservice.service;
+
+import com.spring.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+
+}
